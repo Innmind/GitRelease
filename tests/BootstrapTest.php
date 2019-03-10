50311066413b5209be1038ba618797c712f1c72f
Baptiste Langlade <baptouuuu@gmail.com>
2019-03-10 16:46:00 +0100

    add bootstrap function

---
+++ tests/BootstrapTest.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\GitRelease;
+
+use function Innmind\GitRelease\bootstrap;
+use Innmind\OperatingSystem\OperatingSystem;
+use Innmind\CLI\Commands;
+use PHPUnit\Framework\TestCase;
+
+class BootstrapTest extends TestCase
+{
+    public function testInvokation()
+    {
+        $this->assertInstanceOf(
+            Commands::class,
+            bootstrap($this->createMock(OperatingSystem::class))
+        );
+    }
+}
